37cfa906e05c97b09fb4f8876a628edda0cf1557
robertocaliz <robertocaliz72@gmail.com>
2023-12-16 06:46:41 +0200

    next auth provider: modify

--- src/contexts/NextAuthContext.tsx
+++ src/contexts/NextAuthContext.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 
-import { TWENTY_SECONDS } from '@/constants';
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 
@@ -13,7 +12,7 @@ type NextAuthSessionProviderProps = {
 
 export default function NextAuthSessionProvider({ children }: NextAuthSessionProviderProps) {
 	return (
-		<SessionProvider refetchInterval={TWENTY_SECONDS}>
+		<SessionProvider>
 			{children}
 		</SessionProvider>
 	);
