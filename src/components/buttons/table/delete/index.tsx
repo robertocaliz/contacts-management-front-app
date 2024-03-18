e7e039d87e03fe08990a79dc0cecaa7116d7a2e3
robertocaliz <robertocaliz72@gmail.com>
2024-03-18 07:17:35 +0200

    alteration: modify table buttons

--- src/components/buttons/table/delete/index.tsx
+++ src/components/buttons/table/delete/index.tsx
@@ -3,20 +3,20 @@ import { useTransition } from 'react';
 import { RiDeleteBinLine } from 'react-icons/ri';
 
 type DeleteButtonProps = {
-	handleDelete: (id: string) => void;
-	id: string;
+    handleDelete: (id: string) => void;
+    id: string;
 };
 
 const DeleteButton = ({ handleDelete, id }: DeleteButtonProps) => {
-	const [isPending, startTransition] = useTransition();
-	return (
-		<button
-			className='flex rounded-full p-3 text-[1.3rem] font-bold text-red-600 hover:bg-red-500 hover:text-white focus:bg-white focus:text-red-600'
-			onClick={() => startTransition(() => handleDelete(id))}
-		>
-			{isPending ? <Spinner loading={isPending} /> : <RiDeleteBinLine />}
-		</button>
-	);
+    const [isPending, startTransition] = useTransition();
+    return (
+        <button
+            className='flex rounded-full p-3 text-[1.3rem] font-bold text-red-600 hover:bg-red-500 hover:text-white hover:duration-700 focus:bg-white focus:text-red-600'
+            onClick={() => startTransition(() => handleDelete(id))}
+        >
+            {isPending ? <Spinner loading={isPending} /> : <RiDeleteBinLine />}
+        </button>
+    );
 };
 
 export default DeleteButton;
--- src/components/buttons/table/update/index.tsx
+++ src/components/buttons/table/update/index.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 import { BsPencil } from 'react-icons/bs';
 
 type UpdateButtonProps = {
-	url: string;
+    url: string;
 };
 
 const UpdateButton = ({ url }: UpdateButtonProps) => {
-	return (
-		<Link
-			className='inline-block rounded-full p-3 text-[1.3rem] font-bold  hover:bg-sky-500 hover:text-white focus:bg-white focus:text-sky-500'
-			href={url}
-		>
-			<BsPencil />
-		</Link>
-	);
+    return (
+        <Link
+            className='inline-block rounded-full p-3 text-[1.3rem] font-bold hover:bg-sky-500  hover:text-white hover:duration-700 focus:bg-white focus:text-sky-500'
+            href={url}
+        >
+            <BsPencil />
+        </Link>
+    );
 };
 
 export default UpdateButton;
