23d3df154e2b8bb53eb74b242e40ed38885e275d
robertocaliz <robertocaliz72@gmail.com>
2024-03-19 06:47:38 +0200

    change: modify password page

--- src/app/(auth)/signup/recover/confirm/page.tsx
+++ src/app/(auth)/signup/recover/confirm/page.tsx
@@ -9,7 +9,11 @@ export default function ConfirmEmailPage() {
     return (
         <Centralize>
             <div className='flex flex-col gap-2 text-center'>
-                <span>Confira o seu email</span>
+                <span>
+                    <strong className='text-gray-700'>
+                        Confira o seu email!
+                    </strong>
+                </span>
                 <span>Você receberá um link para definir nova senha.</span>
             </div>
         </Centralize>
