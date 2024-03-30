8a2303bcb0090467a4ce0d2cbb1fee396697e11c
robertocaliz <robertocaliz72@gmail.com>
2024-03-30 12:04:40 +0200

    change checkbox style

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,4 +1,7 @@
-import { ReactNode, InputHTMLAttributes } from 'react';
+'use client';
+
+import clsx from 'clsx';
+import { ReactNode, InputHTMLAttributes, useState } from 'react';
 
 type CheckBoxProps = {
     label?: string | ReactNode;
@@ -6,13 +9,23 @@ type CheckBoxProps = {
 } & InputHTMLAttributes<HTMLInputElement>;
 
 export const CheckBox = ({ label, ...rest }: CheckBoxProps) => {
+    const [checked, setChecked] = useState(false);
+    const handleChange = () => {
+        setChecked(!checked);
+    };
     return (
         <div className='mt-2'>
             <label className='flex items-center gap-2'>
                 <input
                     {...rest}
                     type='checkbox'
-                    className='h-5 w-5 cursor-pointer appearance-none rounded border-[0.13rem] border-gray-200 outline-none'
+                    className={clsx(
+                        'h-5 w-5 cursor-pointer appearance-none rounded border-[0.13rem] border-gray-200 outline-none',
+                        {
+                            'bg-blue-600': checked,
+                        },
+                    )}
+                    onClick={handleChange}
                 />
                 <span>{label}</span>
             </label>
